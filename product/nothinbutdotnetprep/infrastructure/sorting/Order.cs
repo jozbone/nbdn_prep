432dc3c674451f77fd868c349138f0b757e6a3e1
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-12 13:36:06 -0600

    Adding another sorting challenge

--- product/nothinbutdotnetprep/infrastructure/sorting/Order.cs
+++ product/nothinbutdotnetprep/infrastructure/sorting/Order.cs
@@ -1,34 +1,19 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using nothinbutdotnetprep.collections;
-using nothinbutdotnetprep.infrastructure.searching;
 
 namespace nothinbutdotnetprep.infrastructure.sorting
 {
     public class Order<ItemToOrder>
     {
-        public static Comparer<ItemToOrder> by_descending<PropertyType>(Func<ItemToOrder, PropertyType> property_accessor)
+        public static IComparer<ItemToOrder> by_descending<PropertyType>(
+            Func<ItemToOrder, PropertyType> property_accessor) where PropertyType : IComparable<PropertyType>
         {
-            Comparer<ItemToOrder> comparer = Comparer<ItemToOrder>.Default;
-            return comparer;
-        }
-    }
-
-    public class ItemComparer<ItemToCompare, PropertyType> : Comparer<ItemToCompare>
-    {
-        private Func<ItemToCompare, PropertyType> accessor;
-
-        public ItemComparer(Func<ItemToCompare, PropertyType> property_accessor)
-        {
-            accessor = property_accessor;
+            return new ReverseComparer<ItemToOrder>(by(property_accessor));
         }
 
-        public override int Compare(ItemToCompare x, ItemToCompare y)
+        public static IComparer<ItemToOrder> by<PropertyType>(Func<ItemToOrder, PropertyType> property_accessor) where PropertyType : IComparable<PropertyType>
         {
-            return 0;
+            return new ComparablePropertyComparer<ItemToOrder, PropertyType>(property_accessor);
         }
     }
 }
