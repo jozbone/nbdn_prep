db107fb47534fa9dda8a9cd1875dac1e48aa999d
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-11 18:47:54 -0600

    Continuing with searching

---
+++ product/nothinbutdotnetprep/infrastructure/searching/CriteriaFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nothinbutdotnetprep.infrastructure.searching
+{
+    public class CriteriaFactory<ItemToFilter, PropertyType>
+    {
+        Func<ItemToFilter, PropertyType> property_accessor;
+
+        public CriteriaFactory(Func<ItemToFilter, PropertyType> property_accessor)
+        {
+            this.property_accessor = property_accessor;
+        }
+
+        public Criteria<ItemToFilter> equal_to(PropertyType value)
+        {
+            return equal_to_any(value);
+        }
+
+        public Criteria<ItemToFilter> equal_to_any(params PropertyType[] values)
+        {
+            return
+                new AnonymousCriteria<ItemToFilter>(x => new List<PropertyType>(values).Contains(property_accessor(x)));
+        }
+
+        public Criteria<ItemToFilter> not_equal_to(PropertyType value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- product/nothinbutdotnetprep/infrastructure/searching/Where.cs
+++ product/nothinbutdotnetprep/infrastructure/searching/Where.cs
@@ -4,10 +4,10 @@ namespace nothinbutdotnetprep.infrastructure.searching
 {
     public class Where<ItemToFilter>
     {
-        public static Func<ItemToFilter, PropertyType> has_a<PropertyType>(
+        public static CriteriaFactory<ItemToFilter, PropertyType> has_a<PropertyType>(
             Func<ItemToFilter, PropertyType> property_accessor)
         {
-            return property_accessor;
+            return new CriteriaFactory<ItemToFilter, PropertyType>(property_accessor);
         }
     }
 }
