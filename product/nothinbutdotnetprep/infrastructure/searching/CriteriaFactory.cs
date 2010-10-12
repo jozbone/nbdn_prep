5afaa65ab555269772abdcf8bb4e76e24905af46
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-11 21:01:45 -0600

    Removed uneeded file

--- product/nothinbutdotnetprep/infrastructure/searching/CriteriaFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace nothinbutdotnetprep.infrastructure.searching
-{
-    public interface CriteriaFactory<ItemToFilter, PropertyType>
-    {
-        Criteria<ItemToFilter> equal_to(PropertyType value);
-        Criteria<ItemToFilter> equal_to_any(params PropertyType[] values);
-        Criteria<ItemToFilter> not_equal_to(PropertyType value);
-    }
-}
