9dbc82d3ce13c65eaefa828469d5564dffb94086
stpremkumar <stpremkumar@yahoo.com>
2010-10-12 05:57:32 +0530

    Test

--- product/nothinbutdotnetprep/infrastructure/searching/RandomExtensions.cs
+++ product/nothinbutdotnetprep/infrastructure/searching/RandomExtensions.cs
@@ -9,5 +9,11 @@ namespace nothinbutdotnetprep.infrastructure.searching
         {
             return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_equal));
         }
+
+        public static Criteria<ItemToFilter> equal_to_any<ItemToFilter, PropertyType>(this Func<ItemToFilter, PropertyType> property_accessor,
+    PropertyType value_to_equal1, PropertyType value_to_equal2)
+        {
+            return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_equal1) || property_accessor(x).Equals(value_to_equal2));
+        }
     }
 }
